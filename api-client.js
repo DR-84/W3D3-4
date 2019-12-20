d5964010cdd4863c745fe73434742c19cdf5aa70
DR-84 <58080204+DR-84@users.noreply.github.com>
2019-12-20 19:59:18 +0100

    added opdracht

--- api-client.js
+++ api-client.js
@@ -1,12 +1,25 @@
 const API_KEY = '5d512a07a1dfeb577fbda97e63d94d29';
+
 const getData = async function() {
 	const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
 	try {
 		const res = await fetch(apiUrl, { method: 'GET' });
-		console.log('The response from the API in getData function ', res);
 		const data = await res.json();
-		console.log('The data in getData function ', data);
+		console.log('the response from the API URI in getData function is: ', res);
+		console.log('The data in getData function ', typeof data);
+		return data;
 	} catch (error) {
 		console.log(error);
 	}
 };
+
+const getMovieById = async movieName => {
+	const apiUrl = `https://api.themoviedb.org/3/find/${movieName}?external_source=imdb_id&api_key=${API_KEY}`;
+	try {
+		const res = await fetch(apiUrl, { method: 'GET' });
+		return await res.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
+getData();
--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-//
 const movieData = async function() {
 	const data = await getData();
 	console.log('Log ALL the Data!!', data);
+	return data;
 };
 
-document.addEventListener('DOMContentLoaded', event => {
+const setMovieGenres = async () => {
+	const movieGenresList = document.getElementById('list');
+	const data = await movieData();
+	const movieGenres = data.genres;
+	movieGenres.forEach(movieGenre => {
+		const li = document.createElement('li');
+		const liContent = document.createTextNode(
+			`genre naam: ${movieGenre.name},id: ${movieGenre.id}`
+		);
+		li.appendChild(liContent);
+		movieGenresList.append(li);
+	});
+};
+
+const setFavMovie = async () => {
+	const data = await getMovieById('tt0080684');
+	const movie = data.movie_results[0];
+	const para = document.getElementById('empire');
+	para.appendChild(document.createTextNode(movie.title));
+};
+
+document.addEventListener('DOMContentLoaded', () => {
 	console.log('DOM fully loaded and parsed');
 	movieData();
+	setMovieGenres();
+	setFavMovie();
 });
